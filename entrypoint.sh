8f190be7f10c11e432ea869530544ac8dc1ce829
Martijn de Leij <martijn.deleij@avrotros.nl>
2020-06-13 17:08:30 +0200

    Replace crlf with lf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
+#!/bin/sh 
 
-# Start the run once job.
-echo "Docker container has been started"
+# Start the run once job. 
+echo "Docker container has been started" 
 
-# Setup a cron schedule
-echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
+# Setup a cron schedule 
+echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1 # This extra line makes it a valid cron" > scheduler.txt 
 
-crontab scheduler.txt
-cron -f
+crontab scheduler.txt cron -f
