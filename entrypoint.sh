b1caa7dabc8115df55fe5a445d3d13f8a047bb1c
Martijn de Leij <martijn.deleij@avrotros.nl>
2020-06-13 15:55:56 +0200

    Added dockerfile and entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start the run once job.
+echo "Docker container has been started"
+
+# Setup a cron schedule
+echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
