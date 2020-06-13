b4a4b6add79d5cf2598019e3704c673244e9b506
martijndl <martijn@deleij.nl>
2020-06-13 21:55:57 +0200

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@
 echo "Docker container has been started"
 
 # Setup a cron schedule
-echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
+echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1" > scheduler.txt
+
+echo "Contents of scheduler.txt:"
+cat scheduler.txt
 
 echo "Creating crontab"
 crontab scheduler.txt
