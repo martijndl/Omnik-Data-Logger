ebf4d002425c066dd1e5dce4f529b24004fe3c92
Martijn de Leij <martijn.deleij@avrotros.nl>
2020-06-13 17:10:26 +0200

    Line edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 echo "Docker container has been started" 
 
 # Setup a cron schedule 
-echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1 # This extra line makes it a valid cron" > scheduler.txt 
+echo "5 * * * * /usr/bin/python /home/Omnik-Data-Logger/OmnikExport.py >> /var/log/cron.log 2>&1 
+# This extra line makes it a valid cron" > scheduler.txt 
 
 crontab scheduler.txt cron -f
